f420a687aa2cf0cf76040278fc5f58ac95612b09
Kevin <varghese.kevin.john@gmail.com>
2025-03-10 20:27:51 -0400

    Update mergeTwoSortedList.py
    merge2Sort complete

--- mergeTwoSortedList.py
+++ mergeTwoSortedList.py
@@ -10,6 +10,26 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        rlist = []
         
-            
-            
+        while True:
+            if list1.value < list2.value:
+                rlist.append(list1.value)
+                if list1.next == None:
+                    while True:
+                        if list2.next != None:
+                           rlist.append(list2.value)
+                           list2 = list2.next
+                        else:
+                            rlist.append(list2.value)
+                            return(rlist)
+                else:
+                    list1 = list1.next
+            elif list1.value > list2.value:
+                rlist.append(list2.value)
+                if list2.next == None:
+                    while True:
+                        if list1.next != None:
+                            rlist.append(list1.value)
+                            list1 = list1.next
+                        else
