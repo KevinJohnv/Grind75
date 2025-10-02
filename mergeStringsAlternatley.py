adf4768dc4b4b85d3e32f04280653ed287e612f9
KevinJohnv <varghese.kevin.john@gmail.com>
2025-10-01 20:43:16 -0700

    Pushing temp

---
+++ mergeStringsAlternatley.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        m1=0
+        m2=0
+        rstring =""
+        flag=True
+        while len(rstring) < len(word1)+len(word2):
+            if flag:
+                if m1 < len(word1):
+                    rstring= rstring+(word1[m1])
+                    m1 +=1
+                else:
+                    if m2 < len(word2):
+                        rstring= rstring+ (word2[m2:len(word2)])
+                        return rstring
+                    else:
+                        return rstring
+                flag = False
+            else:
+                if m2 < len(word2):
+                    rstring=rstring+(word2[m2])
+                    m2+=1
+                else:
+                    if m1<len(word1):
+                        rstring= rstring+(word1[m1:len(word1)])
+                        return rstring
+                    else:
+                        return rstring
+                flag = True
