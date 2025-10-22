ba3c8e234ea5fe1a7bf16e6100e414b641448e97
KevinJohnv <varghese.kevin.john@gmail.com>
2025-10-21 18:00:23 -0700

    This version of merge alterntately uses list instead of strings.
    Old: O(n^2)
    New: O(n)

--- mergeStringsAlternatley.py
+++ mergeStringsAlternatley.py
@@ -5,33 +5,14 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        m1=0
-        m2=0
-        rstring =""
-        flag=True
-        while len(rstring) < len(word1)+len(word2):
-            print("Reached while + Current:"+rstring)
-            if flag:
-                if m1 < len(word1):
-                    rstring= rstring+(word1[m1])
-                    m1 +=1
-                else:
-                    if m2 < len(word2):
-                        rstring= rstring+ (word2[m2:len(word2)])
-                        return rstring
-                    else:
-                        return rstring
-                flag = False
-            else:
-                if m2 < len(word2):
-                    rstring=rstring+(word2[m2])
-                    m2+=1
-                else:
-                    if m1<len(word1):
-                        rstring= rstring+(word1[m1:len(word1)])
-                        return rstring
-                    else:
+        output =[]
+        min_len = min(len(word1),len(word2))
+        
+        for i in range(min_len):
+            output.append(word1[i])
+            output.append(word2[i])
+        
+        output.append(word1[min_len:])
+        output.append(word2[min_len:])
 
-                        return rstring
-                flag = True
-        return rstring
+        return ''.join(output)
