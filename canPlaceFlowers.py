12982e134b7e26219d9aa33b77fb04a6c74ec4d2
KevinJohnv <varghese.kevin.john@gmail.com>
2025-11-05 20:42:46 -0800

    Bad solution

--- canPlaceFlowers.py
+++ canPlaceFlowers.py
@@ -3,19 +3,25 @@ class Solution:
         prev = 0
         count = 0
         print("Im here")
+        if count >= n:
+            return True
         for i in range(len(flowerbed)):
             if flowerbed[i] == 1:
                 print("Found flower")
                 prev = 1
-            elif i == len(flowerbed):
+            elif i == len(flowerbed)-1:
                 print("Reached end")
+                if prev ==0 and count+1 >=n:
+                    return True
                 return False
             elif flowerbed[i+1]==0:
                 if flowerbed[i] == 0 and prev == 0:        
                     prev = 1
                     count +=1
-                    print("Found space count ="+count)
+                    print("Found space count =",count)
                     if count >= n:
                         return True
+                else:
+                    prev = 0
         return False 
         
