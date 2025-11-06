c78cdc0aa894394e761c4be6abb5855438e49002
KevinJohnv <varghese.kevin.john@gmail.com>
2025-11-05 20:33:06 -0800

    modifying result

--- canPlaceFlowers.py
+++ canPlaceFlowers.py
@@ -1,13 +1,21 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-         
-        
+        prev = 0
+        count = 0
+        print("Im here")
         for i in range(len(flowerbed)):
-            count=0
-            if flowerbed[i-1] == 0 and flowerbed[i+1]==0:
-                flowerbed[I] = 1
-                count +=1
-                if count >= n:
-                    return True
+            if flowerbed[i] == 1:
+                print("Found flower")
+                prev = 1
+            elif i == len(flowerbed):
+                print("Reached end")
+                return False
+            elif flowerbed[i+1]==0:
+                if flowerbed[i] == 0 and prev == 0:        
+                    prev = 1
+                    count +=1
+                    print("Found space count ="+count)
+                    if count >= n:
+                        return True
         return False 
         
